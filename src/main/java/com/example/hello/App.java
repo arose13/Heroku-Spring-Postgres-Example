b3d42ace70148f2718081bff1fee614a2c2115b0
arose13 <anthony_rose12@hotmail.com>
2015-09-27 12:49:43 -0400

    based on https://github.com/ramnivas/hello-spring-cloud

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
 
 /**
@@ -46,6 +47,9 @@ import java.util.ArrayList;
 public class App implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(App.class);
+
+    @Autowired(required = false)
+    DataSource dataSource;
 
     @Autowired
     PersonRepo repo;
