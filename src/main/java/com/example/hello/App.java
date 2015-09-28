9660e2555f329a74af5c417c1fe18144b871b065
arose13 <anthony_rose12@hotmail.com>
2015-09-27 23:19:24 -0400

    gradle generated pom.xml test

--- src/main/java/com/example/hello/App.java
+++ src/main/java/com/example/hello/App.java
@@ -7,6 +7,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.app.ApplicationInstanceInfo;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
