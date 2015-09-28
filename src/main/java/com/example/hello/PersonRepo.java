040bc3b6abace17252e31501df80ffc019c649f2
arose13 <anthony_rose12@hotmail.com>
2015-09-28 19:27:25 -0400

    Added @Repository

--- src/main/java/com/example/hello/PersonRepo.java
+++ src/main/java/com/example/hello/PersonRepo.java
@@ -1,9 +1,11 @@
 package com.example.hello;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PersonRepo extends CrudRepository<Person, Long> {
 
     List<Person> findByNameIgnoreCase(String name);
