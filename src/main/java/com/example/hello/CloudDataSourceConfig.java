26fe2809512830a93a56397a103d54e27ac4c8c6
arose13 <anthony_rose12@hotmail.com>
2015-09-30 16:26:11 -0400

    setDriverClassName

--- src/main/java/com/example/hello/CloudDataSourceConfig.java
+++ src/main/java/com/example/hello/CloudDataSourceConfig.java
@@ -36,6 +36,7 @@ public class CloudDataSourceConfig extends AbstractCloudConfig {
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
         BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setDriverClassName("org.postgresql.Driver");
         basicDataSource.setUrl(dbUrl);
         basicDataSource.setUsername(username);
         basicDataSource.setPassword(password);
